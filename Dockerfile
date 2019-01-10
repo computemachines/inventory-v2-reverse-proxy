6b4abb8c5b79e3b376234fbbaeaba23a1d5a8fa0
Tyler Parker <tparker@computemachines.com>
2019-01-10 09:41:21 -0500

    Stop generating certs within image. Use certs from external volume.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM alpine
 
 RUN apk update && apk add nginx bash curl openssl ca-certificates acme-client libressl git socat
-RUN git clone https://github.com/Neilpang/acme.sh.git
-RUN cd /acme.sh/; ./acme.sh --install
+RUN openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 
 RUN rm -v /etc/nginx/conf.d/default.conf
 
@@ -11,7 +10,6 @@ COPY start_nginx.sh /bin/
 
 RUN mkdir -pv /run/nginx
 
-RUN openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 
 CMD ["sh", "/bin/start_nginx.sh"]
 
--- start_nginx.sh
+++ start_nginx.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-/root/.acme.sh/acme.sh --issue --standalone -d computemachines.com -k 4096
-mkdir -pv /etc/ssl/acme/computemachines.com/
-/root/.acme.sh/acme.sh --installcert -d computemachines.com \
-		       --keypath /etc/ssl/acme/computemachines.com/privkey.key --fullchainpath /etc/ssl/acme/computemachines.com/fullchain.cer
+# /root/.acme.sh/acme.sh --issue --standalone -d computemachines.com -k 4096
+# mkdir -pv /etc/ssl/acme/computemachines.com/
+# /root/.acme.sh/acme.sh --installcert -d computemachines.com \
+# 		       --keypath /etc/ssl/acme/computemachines.com/privkey.key --fullchainpath /etc/ssl/acme/computemachines.com/fullchain.cer
 nginx -g 'daemon off;'
